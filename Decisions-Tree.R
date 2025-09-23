5dba4895b048a7a4513b0ea94d6afb5c066b66cf
Kameron Carter <144400808+KameronCarter@users.noreply.github.com>
2025-09-23 14:49:42 -0400

    Add files via upload

---
+++ Decisions-Tree.R
@@ -0,0 +1,14 @@
+# Author: Kameron Carter Date: 9/23/25 Purpose: Test decision trees in R
+
+install.packages("rpart")
+library(rpart)
+
+#Load some dummy dataset
+kyphosis <- kyphosis
+
+#Create a decision tree
+fit <- rpart(Kyphosis ~ Age + Number + Start, method="class", data= kyphosis)
+
+#Plot the result
+plot(fit, main="Classification plot for Kyphosis", uniform=TRUE)
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
